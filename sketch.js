5810a9243e6d461faa108610e10cd01363efab9c
aprilypop <dtthong2003@gmail.com>
2025-11-17 15:17:42 -0500

    no more commit please

--- sketch.js
+++ sketch.js
@@ -13,7 +13,6 @@ let arrFruitS = [];
 let arrFruitM = [];
 let arrFruitL = [];
 let tapCount = 0;
-let touch;
 
 let font, font2, fruit1, fruit2, fruit3, fruit4, fruit5, fruit6, fruit7, fruit8, fruit9;
 
@@ -63,17 +62,18 @@ function draw() {
   textFont(font);
   textLeading(28);
   textSize(30);
-  text('Touch: '+touch, 20, 45);
+  text('Touch: '+touches.length, 20, 45);
 
 }
 
-function mouseReleased(){
+function mousePressed(){
   generateRope(1, mouseX, mouseY);
 }
 
 function touchEnded(event) {
   // Code to run that uses the event.
   touch = touches[touches.length()-1];
+  generateRope(1, touch.x, touch.y);
 }
 
 function mouseMoved(){
